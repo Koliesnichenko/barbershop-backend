c74d0a0ae14465bd986eae31db290047307f8300
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-05-26 11:30:24 +0200

    refactor: changed schemas to snake_case

--- src/app/schemas.py
+++ src/app/schemas.py
@@ -1,15 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 
 
 class AppointmentCreate(BaseModel):
     name: str
-    phoneNumber: str
-    barberId: int
-    serviceId: int
-    addonIds: List[int]
-    totalDuration: int
-    totalPrice: str
+    phone_number: str = Field(..., alias="phoneNumber")
+    barber_id: int = Field(..., alias="barberId")
+    service_id: int = Field(..., alias="serviceId")
+    addon_ids: List[int] = Field(..., alias="addonIds")
+    total_duration: int = Field(..., alias="totalDuration")
+    total_price: str = Field(..., alias="totalPrice")
+
+    class Config:
+        allow_population_by_field_name = True
 
 
 class AppointmentRead(BaseModel):
