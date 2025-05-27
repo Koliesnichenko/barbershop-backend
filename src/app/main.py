a81f40493f1c49ab4c4917a68f0238cd95f4ed34
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-05-27 13:58:46 +0200

    feat: add basic CORS settings for frontend integration

--- src/app/main.py
+++ src/app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from src.app.routers import appointments
 from src.app.auth.router import router as auth_router
 from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(appointments.router, prefix="/api/appointments", tags=["Appointments"])
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
