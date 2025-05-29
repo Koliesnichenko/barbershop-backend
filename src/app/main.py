0b24ffd4b0ca357aebf9c1d3b0be3161a0f48a9a
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-05-30 00:36:09 +0200

    implemented Addon crud, routers, and schemas. Updated main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.app.routers import barbers, services, appointments
+from src.app.routers import barbers, services, appointments, addons
 from src.app.auth.router import router as auth_router
 from fastapi.openapi.utils import get_openapi
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,6 +18,7 @@ app.include_router(appointments.router, prefix="/api/appointments", tags=["Appoi
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(services.router, prefix="/api/services", tags=["Services"])
 app.include_router(barbers.router, prefix="/api/barbers", tags=["Barbers"])
+app.include_router(addons.router, prefix="/api/addons", tags=["Addons"])
 
 
 def custom_openapi():
