9211afff53bbdb0a59c91226f1af3491f198df5e
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-06-12 19:07:17 +0200

    minor refactor for CORS in main.py

--- src/app/main.py
+++ src/app/main.py
@@ -12,9 +12,16 @@ app = FastAPI(
     default_response_class=ORJSONResponse
 )
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://192.168.10.188:3000/",
+    "http://172.31.48.1:3000/"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://app.example.com"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
